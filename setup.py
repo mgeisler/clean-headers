e87fd4d584541337bb0660279d7449e83a3eaab0
Martin Geisler <martin@geisler.net>
2014-06-01 14:52:51 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Clean File Headers
+"""
+
+from distutils.core import setup
+
+setup(name='clean-headers',
+      version='0.1',
+      maintainer='Martin Geisler',
+      maintainer_email='martin@geisler.net',
+      url='https://github.com/mgeisler/clean-headers',
+      description='Clean File Headers',
+      platforms=['any'],
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3'],
+      scripts=['clean'])
